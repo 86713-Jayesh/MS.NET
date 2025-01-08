0d7e4f79d101cc1b48a3ce046388584fa8713693
Jayesh139 <jkwagh2017@gmail.com>
2025-01-08 22:12:25 +0530

    Assignment

---
+++ Assignment-01/Question 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("useage Calculator <number1> <operation> <number2>");
+                return;
+            }
+
+            double num1 = Convert.ToDouble(args[0]);
+            string operation = args[1];
+            double num2 = Convert.ToDouble(args[2]);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Error Division By Zero");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operations");
+                    return;
+
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ Assignment-01/Question 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuDrivenCalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueCalc = true;
+            do
+            {
+                Console.WriteLine("Enter The First Number");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter The Operations (+ _ / * )");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Enter The Secound Number");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = 0;
+                bool validOperation = true;
+
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Operations");
+                        validOperation = false;
+                        break;
+                }
+
+                if (validOperation)
+                    Console.WriteLine($"Result:{result}");
+            }
+            while (continueCalc);
+        }
+    }
+}
